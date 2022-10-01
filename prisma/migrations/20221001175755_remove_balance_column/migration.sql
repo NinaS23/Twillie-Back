56e4da0a83a5204219d0feba92b1d7f46821ed7f
MariaDuda <mariabevenuto062@gmail.com>
2022-10-01 15:09:29 -0300

    [Fix] wallet table

---
+++ prisma/migrations/20221001175755_remove_balance_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `balance` on the `wallet` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "wallet" DROP COLUMN "balance";
---
+++ prisma/migrations/20221001180347_add_description_column/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "wallet" ADD COLUMN     "description" TEXT NOT NULL DEFAULT 'transection';
