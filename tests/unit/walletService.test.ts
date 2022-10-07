a46af9dd937905181cee6909c9ad5804d9a15e12
MariaDuda <mariabevenuto062@gmail.com>
2022-10-07 13:34:34 -0300

    [Feat] delete wallet register

--- tests/unit/walletService.test.ts
+++ tests/unit/walletService.test.ts
@@ -1,7 +1,9 @@
 import { jest } from "@jest/globals";
 import * as walletService from "../../src/services/walletService";
-import * as  balanceRepository from "../../src/repositories/balanceRepository"
-import { insertBalance } from "../../src/repositories/balanceRepository";
+import * as  balanceRepository from "../../src/repositories/balanceRepository";
+import * as authRepository from "../../src/repositories/authRepository";
+import * as authFactory from "./Factory/authFactory";
+import * as authService from "../../src/services/authService";
 import * as walletRepository from "../../src/repositories/walletRepository";
 import * as walletFactory from "./Factory/walletFactory";
 import { faker } from "@faker-js/faker";
@@ -57,46 +59,120 @@ describe('Test GET /wallet', () => {
         jest
             .spyOn(walletRepository, 'registerData').mockImplementationOnce((): any => {
                 return {
-                    "userId": 1,
-                    "fixedEntry": 1,
-                    "variableEntry": 0,
-                    "fixedOutput": 0,
-                    "variableOutput": 0,
-                    "description": "ganhei dinheiro da vó",
-                    "balancevalue": 2
+                    userId: 1,
+                    fixedEntry: 1,
+                    variableEntry: 0,
+                    fixedOutput: 0,
+                    variableOutput: 0,
+                    description: "ganhei dinheiro da vó",
+                    balancevalue: 2
                 }
             })
         jest
             .spyOn(balanceRepository, 'insertBalance').mockImplementationOnce((): any => {
                 return {
-                    "userId": 1,
-                    "balance": 2,
-                    "walletId": walletId
+                    userId: 1,
+                    balance: 2,
+                    walletId: walletId
                 }
             })
             jest
             .spyOn(walletRepository, 'getRegister').mockImplementationOnce((): any => {
                 return {
-                    "userId": 1,
-                    "fixedEntry": 1,
-                    "variableEntry": 0,
-                    "fixedOutput": 0,
-                    "variableOutput": 0,
-                    "description": "ganhei dinheiro da vó",
-                    "balancevalue": 2
+                    userId: 1,
+                    fixedEntry: 1,
+                    variableEntry: 0,
+                    fixedOutput: 0,
+                    variableOutput: 0,
+                    description: "ganhei dinheiro da vó",
+                    balancevalue: 2
                 }
             })
             
         await walletService.createRegister(inputData, userId);
         expect(walletRepository.registerData).toBeCalled()
-        expect(balanceRepository.insertBalance).toBeCalled()
-        
-    
-        
+        expect(balanceRepository.insertBalance).toBeCalled()  
     })
 
 });
 
 describe('Test DELETE /wallet/:id', () => {
-    it.todo('do a login for user with the correct input data , should return 200')
+    it('do a login for user with the correct input data , should return 200',async () => {
+        const inputData = walletFactory.createRegister();
+        const userId = 1;
+        const walletId = 1
+        const user = authFactory.userData("incorrect");
+        jest.spyOn(authRepository, "isEmailExistent")
+            .mockResolvedValueOnce(null);
+        jest.spyOn(authRepository, "insertUser")
+            .mockImplementationOnce((): any => { });
+        await authService.createUser(user);
+
+        jest
+            .spyOn(walletRepository, 'registerData').mockImplementationOnce((): any => {
+                return {
+                    userId: 1,
+                    "fixedEntry": 1,
+                    variableEntry: 0,
+                    fixedOutput: 0,
+                    variableOutput: 0,
+                    description: "ganhei dinheiro da vó",
+                    balancevalue: 2
+                }
+            })
+        jest
+            .spyOn(balanceRepository, 'insertBalance').mockImplementationOnce((): any => {
+                return {
+                    userId: 1,
+                    balance: 2,
+                    walletId: walletId
+                }
+            })
+        jest
+            .spyOn(walletRepository, 'getRegister')
+            .mockImplementationOnce((): any => {
+                return {
+                    userId: 1,
+                    fixedEntry: 1,
+                    variableEntry: 0,
+                    fixedOutput: 0,
+                    variableOutput: 0,
+                    description: "ganhei dinheiro da vó",
+                    balancevalue: 2
+                }
+            })
+
+        jest
+            .spyOn(authRepository, 'findUserById')
+            .mockResolvedValueOnce({ ...user, id: 1, createdAt: faker.date.recent() });
+
+        jest
+            .spyOn(walletRepository, 'getWalletData')
+            .mockImplementationOnce((): any => {
+                return {
+                    userId: 1,
+                    "fixedEntry": 1,
+                    variableEntry: 0,
+                    fixedOutput: 0,
+                    variableOutput: 0,
+                    description: "ganhei dinheiro da vó",
+                    balancevalue: 2
+                }
+            })
+        jest
+            .spyOn(balanceRepository, "deleteBalance")
+            .mockImplementationOnce((): any => { });
+
+        jest
+            .spyOn(walletRepository, "deleteWalletRegister")
+            .mockImplementationOnce((): any => { });
+
+        await walletService.createRegister(inputData, userId);
+        await walletService.deleteWalletRegister(userId,walletId);
+       
+        expect(walletRepository.deleteWalletRegister).toBeCalled()
+        expect(balanceRepository.deleteBalance).toBeCalled()
+      
+
+    });
 });
