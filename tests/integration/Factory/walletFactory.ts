4998faa81f0e2f52d9c2969631625b5e5b523837
MariaDuda <mariabevenuto062@gmail.com>
2022-10-08 21:58:34 -0300

    [Feat] delete register integration test

--- tests/integration/Factory/walletFactory.ts
+++ tests/integration/Factory/walletFactory.ts
@@ -1,28 +1,6 @@
 import { prisma } from "../../../src/config/database";
 import { insertWalletData } from "../../../src/types/walletTypes";
 
-export async function registerData(wallet:insertWalletData) {
-   const result = await prisma.wallet.create({data:wallet})
-   return result;
-}
-
-
-export async function getRegister(userId:number) {
-  const result =  await prisma.wallet.findMany({ where: {  userId } });
-  return result;
-}
-
-
-export async function getWalletData(id:number) {
- const result = await prisma.wallet.findFirst({ where: {  id } });
- return result;
-  
-}
-
-export async function deleteWalletRegister(id:number) {
-  await prisma.wallet.delete({ where: {  id } });
-  
-}
 
 export function createRegister() {
     return {
--- tests/integration/walletTest.test.ts
+++ tests/integration/walletTest.test.ts
@@ -77,3 +77,52 @@ describe('Test GET /wallet ', () => {
         expect(getRegister.body[0].variableOutput).toEqual(walletInput.variableOutput)
     });
 });
+
+
+describe('Test DELETE /wallet ', () => {
+    it('DELETE first  register , should return 200',async () => {
+        const userInput = authFactory.dataInput();
+        const isUserExistent = await authFactory.isEmailExistent(userInput.email);
+        expect(isUserExistent).toBeNull();
+        const createUser = await server
+            .post("/sing-up")
+            .send(userInput);
+        const getUserInput = await authFactory.isEmailExistent(userInput.email);
+        expect(getUserInput).not.toBeNull();
+        expect(createUser.statusCode).toBe(201);
+       
+        const findUserByEmail = await authFactory.isEmailExistent(userInput.email);
+        expect(findUserByEmail).not.toBeNull();
+        const loginUser = await server
+            .post("/sing-in")
+            .send({ email: userInput.email, password: userInput.password });
+        expect(loginUser.statusCode).toBe(200);
+        expect(loginUser.body.token).not.toBeNull();
+
+        const token = loginUser.body.token;
+        const walletInput = walletFactory.createRegister()
+        const createRegister = await server
+        .post("/wallet")
+        .send(walletInput).set("Authorization", `Bearer ${token}`);;
+        expect(createRegister.statusCode).toBe(201);
+
+        const getRegister = await server
+        .get("/wallet")
+        .send().set("Authorization", `Bearer ${token}`);
+        expect(getRegister.statusCode).toBe(200);
+        expect(getRegister.body[0].fixedEntry).toEqual(walletInput.fixedEntry)
+        expect(getRegister.body[0].variableEntry).toEqual(walletInput.variableEntry)
+        expect(getRegister.body[0].fixedOutput).toEqual(walletInput.fixedOutput)
+        expect(getRegister.body[0].variableOutput).toEqual(walletInput.variableOutput)
+
+        const deleteRegister = await server
+        .delete("/wallet/1")
+        .send().set("Authorization", `Bearer ${token}`);
+        expect(deleteRegister.statusCode).toBe(200)
+        
+        const getNothing = await server
+        .get("/wallet")
+        .send().set("Authorization", `Bearer ${token}`);
+        expect(getNothing.body).toEqual({});
+    });
+});
