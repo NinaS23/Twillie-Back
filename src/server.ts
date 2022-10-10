cc79cbe041eed055ff799b31aafad2e171272f88
MariaDuda <mariabevenuto062@gmail.com>
2022-10-10 14:28:44 -0300

    [Feat] deploy

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import app from "./app/app";
 
-app.listen(process.env.PORT, () => {
-    console.log("Server running on port " + process.env.PORT);
+const port = process.env.PORT || 6003
+app.listen(port, () => {
+    console.log("Server running on port " + port);
 });
