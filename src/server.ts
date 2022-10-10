1a73c6464b11145b67677c3956b153df56ee6e51
MariaDuda <mariabevenuto062@gmail.com>
2022-10-10 14:09:55 -0300

    deploy at heroky

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,5 @@
 import app from "./app/app";
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const port = +process.env.PORT || 6002;
-
-app.listen(port, () => {
-    console.log(`Server is up and running on port ${port}`)
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
 });
